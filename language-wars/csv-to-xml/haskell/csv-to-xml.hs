ca00674ba674dbb3dffe81e9f685a31957b63ef8
scox <scox@pervasive.com>
2013-01-20 19:58:03 -0600

    Better name for line -> record function.

--- language-wars/csv-to-xml/haskell/csv-to-xml.hs
+++ language-wars/csv-to-xml/haskell/csv-to-xml.hs
@@ -1,9 +1,9 @@
 import Data.List.Split
 
 main = do putStrLn "<records>"
-          interact $ unlines . (map toElem) . lines
+          interact $ unlines . (map toRecord) . lines
           putStrLn "</records>"
 
-toElem l = "  <record>\n" ++ (unlines $ toFields l) ++ "  </record>"
+toRecord l = "  <record>\n" ++ (unlines $ toFields l) ++ "  </record>"
 toFields s = map toField $ splitOn "," s
 toField v = "   <field>" ++ v ++ "</field>"
